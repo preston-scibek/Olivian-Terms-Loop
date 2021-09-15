22b59e9cb4624743d03d63878122f76a8e187789
Preston Scibek <preston@bytelion.com>
2021-09-15 15:28:56 -0400

    Adding Olivian-Terms-Loop

---
+++ README.md
@@ -0,0 +1,28 @@
+# Olive Helps: Olive-Terms-Loop
+
+Olivology - A loop that allows you to look up any and every Olive vocabulary word. Sources could include https://www.sllane.com/olivology
+
+## Requirements
+
+You will need to install [Olive Helps](https://oliveai.com/olive-helps/).
+
+Install [Node.js](https://nodejs.org/), clone this repo, and install the project dependencies with:
+```shell
+npm install
+```
+
+Then compile and package the project with:
+```shell
+npm run build
+```
+
+## Other Commands
+
+```shell
+# ESLint and Prettier
+npm run lint          # Check for issues
+npm run lint:fix      # Fix all auto-fixable issues
+
+# Watch
+npm run watch          # Watch for changes and reubild the loop as needed
+```
---
+++ src/index.js
@@ -0,0 +1,62 @@
+const {ui, network, whisper} = require('@oliveai/ldk');
+const cheerio = require('cheerio');
+const { last } = require('cheerio/lib/api/traversing');
+
+const writeWhisper = (labelV, body) => {
+  whisper.create({
+    label: labelV,
+    onClose: () => { console.log('Closed Whisper') },
+    components: [
+        {
+            body: body,
+            type: whisper.WhisperComponentType.Markdown,
+        },
+    ]
+  });
+};
+
+const readFromUrl = async (url) => {
+  const request = {
+    method: 'GET',
+    url: `${url}`,
+  };
+  const response = await network.httpRequest(request);
+  const decodedBody = await network.decode(response.body);
+  return decodedBody;
+}
+
+async function terminologyCallback(incomingText){
+    const url = 'https://sllane.com/olivology';
+    const html = await readFromUrl(url);
+    const $ = cheerio.load(html);
+    let type;
+    if (incomingText.includes(" ")){
+        type = 'phrase';
+    }
+    else{
+        type = 'word';
+    }
+    let answer = `I could not find the meaning of this ${type}`;
+    
+    let myDiv = $('div:contains("Alphabetical Listing of Terms")').last()
+    let res = [];
+    let filtered = $(myDiv).children().filter((i, el) => {
+        const text = $(el).text();
+        const temp = text.split(":");
+        return incomingText.toLowerCase().includes(temp[0].toLowerCase())
+    })
+
+    filtered.each(function (i, e) {
+        let str = $(e).text();
+        res[i] = str;
+    });
+    answer = res.join(",")
+    writeWhisper(`The meaning of ${incomingText}`, `${res}`);
+}
+
+async function terminology(){
+  writeWhisper("Olive Terminology Started", "Search an olivian word to retreive the definition")
+  ui.listenSearchbar(terminologyCallback);
+}
+
+terminology();
