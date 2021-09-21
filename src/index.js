d93687caa8355413d73623e08367f43bc15215c8
Preston Scibek <preston@bytelion.com>
2021-09-21 11:48:43 -0400

    linting file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-const {ui, network, whisper, filesystem} = require('@oliveai/ldk');
+const { ui, network, whisper, filesystem } = require('@oliveai/ldk');
 const cheerio = require('cheerio');
-const { last } = require('cheerio/lib/api/traversing');
 
 const writeWhisper = (labelV, body) => {
   whisper.create({
     label: labelV,
-    onClose: () => { console.log('Closed Whisper') },
+    onClose: () => {
+      console.log('Closed Whisper');
+    },
     components: [
-        {
-            body: body,
-            type: whisper.WhisperComponentType.Markdown,
-        },
-    ]
+      {
+        body,
+        type: whisper.WhisperComponentType.Markdown,
+      },
+    ],
   });
 };
 
@@ -23,64 +24,67 @@ const readFromUrl = async (url) => {
   const response = await network.httpRequest(request);
   const decodedBody = await network.decode(response.body);
   return decodedBody;
-}
+};
 
-async function terminologyCallback(incomingText){
-    const url = 'https://sllane.com/olivology';
-    const webpageSeachStr = "Alphabetical Listing of Terms";
-    const html = await readFromUrl(url);
-    const $ = cheerio.load(html);
-    let type;
-    if (incomingText.includes(" ")){
-        type = 'phrase';
-    }
-    else{
-        type = 'word';
-    }
-    let answer = `I could not find the meaning of this ${type}`;
-    
-    let myDiv = $(`div:contains("${webpageSeachStr}")`).last()
-    let res = [];
-    let addedTracker = [];
+async function terminologyCallback(incomingText) {
+  const url = `https://sllane.com/olivology`;
+  const webpageSeachStr = `Alphabetical Listing of Terms`;
+  const html = await readFromUrl(url);
+  const $ = cheerio.load(html);
+  let type;
+  if (incomingText.includes(` `)) {
+    type = 'phrase';
+  } else {
+    type = 'word';
+  }
+  let answer = `I could not find the meaning of this ${type}`;
+  const myDiv = $(`div:contains("${webpageSeachStr}")`).last();
+  const res = [];
+  const addedTracker = [];
 
-    // todo , see if we can put this data somewhere else (ideally online to be able to dynamically update)
-    //  (or update sllane.com/olivology 
-    // in which case this backup block is redudnant)
-    let backupFile = '/tmp/backup.txt';
-    
-    if (filesystem.exists(backupFile)) {
-      let backupList = await filesystem.readFile(backupFile);
-      const decodedList = await network.decode(backupList);
-      decodedList.split('\n').forEach((row) => {
-        if (row) {
-          const temp = row.split(":");
-          if (temp[0].toLowerCase().includes(incomingText.toLowerCase())){
-            res.push(row)
-            addedTracker.push(temp[0].toLowerCase());
-          }
+  // todo , see if we can put this data somewhere else (ideally online to be able to dynamically update)
+  //  (or update sllane.com/olivology
+  // in which case this backup block is redudnant)
+  const backupFile = '/tmp/backup.txt';
+
+  if (filesystem.exists(backupFile)) {
+    const backupList = await filesystem.readFile(backupFile);
+    const decodedList = await network.decode(backupList);
+    decodedList.split('\n').forEach((row) => {
+      if (row) {
+        const temp = row.split(':');
+        if (temp[0].toLowerCase().includes(incomingText.toLowerCase())) {
+          res.push(row);
+          addedTracker.push(temp[0].toLowerCase());
         }
-      });
-    }
+      }
+    });
+  }
 
-    let filtered = $(myDiv).children().filter((i, el) => {
-        const text = $(el).text();
-        const temp = text.split(":");
-        if (temp[0] === webpageSeachStr){
-          return false;
-        }
-        return temp[0].toLowerCase().includes(incomingText.toLowerCase()) && !addedTracker.includes(temp[0].toLowerCase());
+  const filtered = $(myDiv)
+    .children()
+    .filter((i, el) => {
+      const text = $(el).text();
+      const temp = text.split(':');
+      if (temp[0] === webpageSeachStr) {
+        return false;
+      }
+      return (
+        temp[0].toLowerCase().includes(incomingText.toLowerCase()) &&
+        !addedTracker.includes(temp[0].toLowerCase())
+      );
     });
 
-    filtered.each(function (i, e) {
-        let str = $(e).text();
-        res.push(str);
-    });
-    answer = res.join("     \n");
-    writeWhisper(`The meaning of ${incomingText}`, `${answer}`);
+  filtered.each(function temp(i, e) {
+    const str = $(e).text();
+    res.push(str);
+  });
+  answer = res.join('     \n');
+  writeWhisper(`The meaning of ${incomingText}`, `${answer}`);
 }
 
-async function terminology(){
-  writeWhisper("Olive Terminology Started", "Search an olivian word to retrieve the definition")
+async function terminology() {
+  writeWhisper('Olive Terminology Started', 'Search an olivian word to retrieve the definition');
   ui.listenSearchbar(terminologyCallback);
   ui.listenGlobalSearch(terminologyCallback);
 }
